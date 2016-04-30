019bca747a05277e0c155db2ea39f22c630cfed2
Christopher Sumnicht <csumnicht@berkeley.edu>
2016-04-30 03:31:20 -0700

    iprobe only works locally Iprobe works for both local and savio

--- src/process.py
+++ src/process.py
@@ -199,7 +199,7 @@ class Process:
         Returns None if there is nothing to be recieved.
         """
         # Probe for any sources
-        if self.comm.iprobe(source=MPI.ANY_SOURCE):
+        if self.comm.Iprobe(source=MPI.ANY_SOURCE):
             # If there are sources recieve them.
             self.received.append(self.recv(source=MPI.ANY_SOURCE))
             for job in self.received:
