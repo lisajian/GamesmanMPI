787f1af8c9ed3d39127dce3a3ad9a915fcd5e45d
Christopher Sumnicht <csumnicht@berkeley.edu>
2016-05-16 22:47:37 -0700

    Proto for type_compiler

---
+++ src/type_compiler.py
@@ -0,0 +1,27 @@
+def string_mat(*args):
+    '''
+    Multi-dimensional string type.
+    char[arg1][arg2]...[argn]
+    '''
+    pass
+
+def int_mat(*args):
+    '''
+    Multi-dimensional int type.
+    int[arg1][arg2]...[argn]
+    '''
+    pass
+
+def int_type(*args):
+    '''
+    Single int type.
+    int
+    '''
+    pass
+
+def char_type(*args):
+    '''
+    Single char type.
+    char
+    '''
+    pass
