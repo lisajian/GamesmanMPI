6b0cd4be2dda100b3096163a0414640a381257c8
Christopher Sumnicht <csumnicht@berkeley.edu>
2016-05-22 22:01:58 -0700

    Fixed a couple of flake8 issues in utils.py

--- src/utils.py
+++ src/utils.py
@@ -7,9 +7,11 @@ game_module                     = None # This is initialized in solve_launcher.p
 # Used for logging/display purposes
 STATE_MAP       = {0:"win", 1:"loss", 2:"tie", 3:"draw", 4:"undecided"}
 
+
 def negate(state):
     neg = (1, 0, 2, 3, 4)
     return neg[state]
+
 
 def to_str(state):
     str_rep = ("WIN", "LOSS", "TIE", "DRAW", "UNDECIDED")
